2c976390d99f8b67ed269501fe5ec9089dd50e0b
Jo Alfie Wimborne <jowimborne@gmail.com>
2019-09-16 23:13:00 +0100

    Replaced about with projects page & added page title

--- src/pages/about.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => <Layout metaTitle="Jo Alfie Wimborne">Hello about</Layout>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default () => (
-	<Layout metaTitle="Contact | Jo Alfie Wimborne" title="Contact">
-		Hello contact
+	<Layout metaTitle="Projects | Jo Alfie Wimborne" title="Projects">
+		Hello projects
 	</Layout>
 )
